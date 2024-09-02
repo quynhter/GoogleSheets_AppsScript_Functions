e147d49d2ddc1fb257912a998a55cbc7f89f4e5e
Sergei Cher&Shev <160236147+quynhter@users.noreply.github.com>
2024-09-02 12:01:10 +0300

    Add files via upload

---
+++ parseStructure.js
@@ -0,0 +1,29 @@
+// Output table
+//  | A      | B     | C
+// 1| Number | Place | Deficiencies
+// 2| 1      | ..    | ..
+
+// Input data
+// num = 1 (from output table)
+// cell = [{"number": 1}, {"place": "Classroom"}, {"deficiencies": "Broken tables"}]
+
+function parseStructure(num, cell) {
+  var result = [];
+  var data = JSON.parse(cell);
+  data.forEach(
+    function(element) {
+      if (element.number == num) {
+        result.push(element.place);
+        result.push(element.deficiencies);
+      };
+    }
+  );
+  return result;
+};
+
+// Output data
+// result = ["Classroom", "Broken tables"]
+// If you enter this formula "TRANSPOSE(parseStructure())" in B2, you will get next output table
+//  | A      | B            | C
+// 1| Number | Place        | Deficiencies
+// 2| 1      | Classroom    | Broken tables
